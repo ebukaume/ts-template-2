868479ad6b6f2ccfb0adaebf69b5fec6aa3be59f
Ebuka Umeokonkwo <ebukaume@gmail.com>
2023-06-08 02:12:39 +0200

    fix typescheck

--- src/__fixtures__/user.ts
+++ src/__fixtures__/user.ts
@@ -1,11 +1,14 @@
 import { type User } from '@prisma/client';
 import { randomUUID } from 'crypto';
 
-export function user (params?: Partial<User>): User {
+export function user(params?: Partial<User>): User {
   return {
     id: randomUUID(),
     email: 'default@email.com',
     name: 'Some User',
+    username: 'username',
+    password: 'password',
+    emailConfirmationToken: null,
     ...params
   };
 }
